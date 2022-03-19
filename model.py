2570f4bc55c92da235290e5bb41b77e73a147bdc
elian <elian1203@gmail.com>
2022-03-19 19:38:38 -0400

    Created model for binary attributes to load from text and function to generate all combinations of attributes

---
+++ model.py
@@ -0,0 +1,45 @@
+import itertools
+
+
+class BinaryAttribute:
+    def __init__(self, name, off_value, on_value):
+        self.name = name
+        self.off_value = off_value
+        self.on_value = on_value
+
+    @staticmethod
+    def load_from_line(line):
+        name = line.split(': ')[0]
+        values = line.split(': ')[1]
+
+        off_value = values.split(', ')[0]
+        on_value = values.split(', ')[1]
+
+        return BinaryAttribute(name, off_value, on_value)
+
+
+def load_binary_attributes(attributes_text):
+    attributes = []
+
+    for line in attributes_text.split('\n'):
+        attributes.append(BinaryAttribute.load_from_line(line))
+
+    return attributes
+
+
+def generate_attribute_combinations(attributes):
+    distinct_attributes = []
+
+    # generate distinct attributes
+    for attribute in attributes:
+        distinct_attributes.append((attribute, 'off'))
+        distinct_attributes.append((attribute, 'on'))
+
+    attribute_combinations = itertools.combinations(distinct_attributes, len(attributes))
+
+    combinations_list = []
+
+    for combination in attribute_combinations:
+        combinations_list.append(combination)
+
+    return combinations_list
