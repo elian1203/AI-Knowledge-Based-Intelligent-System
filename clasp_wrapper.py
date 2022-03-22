8f60e4e568754593c5ebc8cb1747112645df6be7
elian <elian1203@gmail.com>
2022-03-22 12:54:47 -0400

    Update clasp wrapper to appropriately identify satisfiability

--- clasp_wrapper.py
+++ clasp_wrapper.py
@@ -2,6 +2,17 @@ import subprocess
 
 
 def clasp(program_input):
+    """
+    Calls clasp command (executable should be in current dir or PATH
+    :param program_input: clasp appropriate input (p cnf 4 6......)
+    :return: True if satisfiable, False if not satisfiable
+    """
     run = subprocess.run(['clasp'], input=program_input.encode(), stdout=subprocess.PIPE)
-    print(run.stdout)
-    return run.stdout
+
+    for line in run.stdout.splitlines():
+        if line.startswith('s SATISFIABLE'):
+            return True
+        elif line.startswith('s UNSATISFIABLE'):
+            return False
+
+    raise Exception('Missing clasp file! Unable to process constraints.')
